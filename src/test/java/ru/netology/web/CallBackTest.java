1c12b64101cd0dac5f14ece5f5198119dd074446
Vurhis1 <denraimond@gmail.ru>
2022-10-20 16:50:24 +0300

    Change CallBackTest

--- src/test/java/ru/netology/web/CallBackTest.java
+++ src/test/java/ru/netology/web/CallBackTest.java
@@ -4,17 +4,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallBackTest {
     private WebDriver driver;
 
+    {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
     @BeforeAll
     static void setupAll() {
         WebDriverManager.chromedriver().setup();
@@ -22,11 +27,7 @@ public class CallBackTest {
 
     @BeforeEach
     void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     @AfterEach
